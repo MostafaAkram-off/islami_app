a7e5ced99381202d326aa985a03a36560d9ec80a
Mostafa <mostafaakram2004@gmail.com>
2026-08-27 04:47:46 +0300

    + Handle quran tab without search and most recent + Show quran details for every sura

--- lib/core/resources/strings_manger.dart
+++ lib/core/resources/strings_manger.dart
@@ -4,4 +4,7 @@ abstract class StringsManger {
   static const String sebha ="Sebha";
   static const String radio ="Radio";
   static const String time = "Time";
+  static const String suraName = "Sura Name";
+  static const String mostRecently  = "Most Recently";
+  static const String surasList  = "Suras List";
 }
